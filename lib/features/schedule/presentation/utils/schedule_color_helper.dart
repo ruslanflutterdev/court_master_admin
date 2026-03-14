9d416d4c572c2a8b83731eb5f1ee722b9bdfb68c
ruslanflutterdev <rusrus1984_8@mail.ru>
2026-03-14 16:14:20 +0500

    обновил ScheduleColorHelper для приоритетного отображения кастомных цветов (colorHex). расширил палитру палитра в ScheduleColorPickerRow, улучшен UI выбора цвета

--- lib/features/schedule/presentation/utils/schedule_color_helper.dart
+++ lib/features/schedule/presentation/utils/schedule_color_helper.dart
@@ -5,6 +5,16 @@ class ScheduleColorHelper {
     if (type == 'maintenance') return Colors.grey.shade600;
     if (type == 'tournament') return Colors.red.shade400;
 
+    if (dbColorHex != null &&
+        dbColorHex.length == 7 &&
+        dbColorHex.startsWith('#')) {
+      try {
+        return Color(
+          int.parse(dbColorHex.substring(1, 7), radix: 16) + 0xFF000000,
+        );
+      } catch (_) {}
+    }
+
     switch (type) {
       case 'individual':
         return Colors.blue;
@@ -13,15 +23,6 @@ class ScheduleColorHelper {
       case 'rent':
         return Colors.orange;
       default:
-        if (dbColorHex != null && dbColorHex.length == 7) {
-          try {
-            return Color(
-              int.parse(dbColorHex.substring(1, 7), radix: 16) + 0xFF000000,
-            );
-          } catch (_) {
-            return Colors.blue;
-          }
-        }
         return Colors.blue;
     }
   }

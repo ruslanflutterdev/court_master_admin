ebff4204b9e851efbaeb9fad520ab9244d9df8d7
ruslanflutterdev <rusrus1984_8@mail.ru>
2026-03-19 23:34:03 +0500

    fix: increase api timeout to 60s

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -17,8 +17,8 @@ class ApiClient {
     dio = Dio(
       BaseOptions(
         baseUrl: baseUrl,
-        connectTimeout: const Duration(seconds: 10),
-        receiveTimeout: const Duration(seconds: 10),
+        connectTimeout: const Duration(seconds: 60),
+        receiveTimeout: const Duration(seconds: 60),
       ),
     );
 
